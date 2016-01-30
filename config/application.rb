bed9007b28409f27ee429fd882dcc054f6d6d7fb
Cu Huu Hoang <huuhoangcu@gmail.com>
2016-01-30 21:39:13 +0700

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module SampleApp
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Hanoi'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -22,5 +22,8 @@ module SampleApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #include the authenticity token in remote forms
+    config.action_view.embed_authenticity_token_in_remote_forms =true
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -89,5 +89,23 @@ class UserTest < ActiveSupport::TestCase
 		assert archer.followers.include?(michael)
 		michael.unfollow(archer)
 		assert_not michael.following?(archer)
-	end
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    #posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    #posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    #posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
